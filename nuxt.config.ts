75c1ae98c5dd8701e70bd45fdf2eb3b51f2894fb
皓月 <zhangwenhaoyue@163.com>
2024-05-13 16:49:04 +0800

    更新

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    // baseURL: "/zhi-blog/",
+    buildAssetsDir: "/zhi-blog/",
+  },
+
   ssr: false,
 
   devtools: { enabled: false },
