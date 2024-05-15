e275bf86bede2d1d9ec216b915b92457d978cafd
皓月 <zhangwenhaoyue@163.com>
2024-05-15 20:01:28 +0800

    fix: 修复移动端页面会横向滚动的问题

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="flex flex-col items-center px-4">
+  <main class="flex flex-col items-center px-4 overflow-x-hidden">
     <AppIntro />
     <AppSectionDivider />
     <AppAbout />
